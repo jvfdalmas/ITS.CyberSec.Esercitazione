c454a257a183f18045d2085cdde420a4bba57332
jvfdalmas <juaaaum@hotmail.com>
2024-04-22 13:15:46 +0200

    parziale

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -124,11 +124,11 @@ print("\n")
 
 """8-9. Messages: Make a list containing a series of short text messages. Pass the list to a function called show_messages(), which prints each text message."""
 
-short_messages: list = ["Hope you are doing fine!", "Grown strong!", "you can do it!"]
+short_messages: list = ["Hope you are doing fine!", "Grow strong!", "you can do it!"]
 
 def print_messages(arg: list) -> str:
     for message in arg:
-        return message
+        print(message)
 
 print_messages(short_messages)
 
@@ -159,34 +159,47 @@ print("\n")
 
 """8-11. Archived Messages: Start with your work from Exercise 8-10. Call the function send_messages() with a copy of the list of messages. After calling the function, print both of your lists to show that the original list has retained its messages."""
 
+send_messages(sent_messages[:])
+print(sent_messages)
+
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n")
-
 
 """8-12. Sandwiches: Write a function that accepts a list of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that’s being ordered. Call the function three times, using a different number of arguments each time."""
 
+sandwiches:list = ['tunna sandwich', 'pastrami sandwich', 'hamburguer', 'cheeseburger','pastrami sandwich', 'cheddar mc melt']
+
+finished_orders = []
+
+while sandwiches:
+        for sandwich in sandwiches:
+            current_order = sandwiches.pop()
+            finished_orders.append(current_order)
+            if 'pastrami' in current_order:
+                  print('Sorry, but we ran out of pastrami') 
+            else:
+                print(f'I am making you a {current_order}!')
+
+print(sandwiches)
+print(finished_orders)
 
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n")
-
 
 """8-13. User Profile:  Build a profile of yourself by calling build_profile(), using your first and last names and three other key-value pairs that describe you. All the values must be passed to the function as parameters. The function then must return a string such as "Eric Crow, age 45, hair brown, weight 67"""
 
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n")
 
-
 """8-14. Cars: Write a function that stores information about a car in a dictionary. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Your function should work for a call like this one: car = make_car('subaru', 'outback', color='blue', tow_package=True) Print the dictionary that’s returned to make sure all the information was stored correctly. """
 
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n")
 
-
 """8-15. Printing Models: Put the functions for the example printing_models.py in a separate file called printing_functions.py. Write an import statement at the top of printing_models.py, and modify the file to use the imported functions."""
 
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n")
-
 
 """ 8-16. Imports: Using a program you wrote that has one function in it, store that function in a separate file. Import the function into your main program file, and call the function using each of these approaches:
 import module_name
@@ -197,6 +210,5 @@ from module_name import * """
 
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n")
-
 
 """8-17. Styling Functions: Choose any three programs you wrote for this chapter, and make sure they follow the styling guidelines described in this section."""
