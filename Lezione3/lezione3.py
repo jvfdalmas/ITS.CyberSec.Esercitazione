55d2b8155cadeb748f40c3f0e40f82da0e953af3
jvfdalmas <juaaaum@hotmail.com>
2024-04-22 11:33:02 +0200

    final

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -2,7 +2,7 @@
 • Modify your for loop to print a sentence using the name of the pizza, instead of printing just the name of the pizza. For each pizza, you should have one line of output containing a simple statement like I like pepperoni pizza.
 • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!"""
 
-pizze = ['margheritta', 'mozzarella', 'fiore di zucca']
+pizze: list = ['margheritta', 'mozzarella', 'fiore di zucca']
 
 for pizza in pizze:
     if pizza == "margheritta":
@@ -20,7 +20,7 @@ print("\n")
 • Modify your program to print a statement about each animal, such as A dog would make a great pet.
 • Add a line at the end of your program, stating what these animals have in common. You could print a sentence, such as Any of these animals would make a great pet!"""
 
-Animals = ["monkey", "gorilla", "ape"]
+Animals: list = ["monkey", "gorilla", "ape"]
 for Animal in Animals:
     print(f"I don't think {Animal} would make a good pet...")
 print("None of these animals would make a good pet...")
@@ -30,7 +30,7 @@ print("\n")
 
 """4-3. Counting to Twenty: Use a for loop to print the numbers from 1 to 20, inclusive."""
 
-countingto20 = [number for number in range(1,21)]
+countingto20: list = [number for number in range(1,21)]
 print(countingto20)
 
 # -------------------------------------------------------------------------------------------------------------------------------
@@ -38,24 +38,24 @@ print("\n")
 
 """4-4. One Million: Make a list of the numbers from one to one million, and then use a for loop to print the numbers. (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)"""
 
-#countingtomillion = [number for number in range(1_000_001)]
-#print(countingtomillion)
+countingtomillion: list = [number for number in range(1_000_001)]
+print(countingtomillion)
 
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n")
 
 """4-5. Summing a Million: Make a list of the numbers from one to one million, and then use min() and max() to make sure your list actually starts at one and ends at one million. Also, use the sum() function to see how quickly Python can add a million numbers."""
 
-#print(min(countingtomillion))
-#print(max(countingtomillion))
-#print(sum(countingtomillion))
+print(min(countingtomillion))
+print(max(countingtomillion))
+print(sum(countingtomillion))
 
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n")
 
 """4-6. Odd Numbers: Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. Use a for loop to print each number."""
 
-Oddnumbers = [number for number in range(1, 21, 2)]
+Oddnumbers: list = [number for number in range(1, 21, 2)]
 print(Oddnumbers)
 
 # -------------------------------------------------------------------------------------------------------------------------------
@@ -63,7 +63,7 @@ print("\n")
 
 """4-7. Threes: Make a list of the multiples of 3, from 3 to 30. Use a for loop to print the numbers in your list."""
 
-multipleofthree = []
+multipleofthree: list = []
 for number in range(3,33):
     if number % 3 == 0:
         multipleofthree.append(number)
@@ -74,9 +74,9 @@ print("\n")
 
 """4-8. Cubes: A number raised to the third power is called a cube. For example, the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that is, the cube of each integer from 1 through 10), and use a for loop to print out the value of each cube."""
 
-cubes = []
+cubes: list = []
 for number in range(1,11):
-    cube = number ** 3
+    cube: int = number ** 3
     cubes.append(cube)
 print(cubes)
 
@@ -85,7 +85,7 @@ print("\n")
 
 """4-9. Cube Comprehension: Use a list comprehension to generate a list of the first 10 cubes."""
 
-Cubes=[number ** 3 for number in range(1,11)]
+Cubes: list = [number ** 3 for number in range(1,11)]
 print(Cubes)
 
 # -------------------------------------------------------------------------------------------------------------------------------
@@ -96,7 +96,7 @@ print("\n")
 • Print the message Three items from the middle of the list are:. Then use a slice to print three items from the middle of the list.
 • Print the message The last three items in the list are:. Then use a slice to print the last three items in the list."""
 
-My_Pizza = ['mozzarella', 'pepperone', 'fior di zucca', 'capriciosa']
+My_Pizza: list = ['mozzarella', 'pepperone', 'fior di zucca', 'capriciosa']
 print(f"The first three pizzas of my list are {', '.join(My_Pizza[0:3])}")
 print(f"The two pizzas in the middle of my list are {', '.join(My_Pizza[1:3])}")
 print(f"The last three pizzas of my list are {', '.join(My_Pizza[-3::])}")
@@ -109,8 +109,8 @@ print("\n")
 • Add a different pizza to the list friend_pizzas.
 • Prove that you have two separate lists. Print the message My favorite pizzas are:, and then use a for loop to print the first list. Print the message My friend’s favorite pizzas are:, and then use a for loop to print the second list. Make sure each new pizza is stored in the appropriate list."""
 
-My_Pizza = ['mozzarella', 'pepperone', 'fior di zucca', 'capriciosa']
-Friends_Pizza = My_Pizza[:]
+My_Pizza: list = ['mozzarella', 'pepperone', 'fior di zucca', 'capriciosa']
+Friends_Pizza: list = My_Pizza[:]
 My_Pizza.append('rucula')
 Friends_Pizza.append('pollo')
 
@@ -126,15 +126,15 @@ print("\n")
 """4-14. PEP 8:  Look through the original PEP 8 style guide at https://peps.python.org/pep-0008/ You won’t use much of it now, but it might be interesting to skim through it."""
 """4-15. Code Review: Choose three of the programs you’ve written in this chapter and modify each one to comply with PEP 8."""
 
-Oddnumbers = [number for number in range(1, 21, 2)]
+Oddnumbers: list = [number for number in range(1, 21, 2)]
 print(Oddnumbers)
 
-animals = ["monkey", "gorilla", "ape"]
+animals: list = ["monkey", "gorilla", "ape"]
 for animal in animals:
     print(f"I don't think {animal} would make a good pet...")
 print("None of these animals would make a good pet...")
 
-my_pizza = ['mozzarella', 'pepperone', 'fior di zucca', 'capriciosa']
+my_pizza: list = ['mozzarella', 'pepperone', 'fior di zucca', 'capriciosa']
 print(f"The first three pizzas of my list are {', '.join(my_pizza[0:3])}")
 print(f"The two pizzas in the middle of my list are {', '.join(my_pizza[1:3])}")
 print(f"The last three pizzas of my list are {', '.join(my_pizza[-3::])}")
@@ -153,7 +153,7 @@ evaluates to True or False.
 • Create at least 10 tests. Have at least 5 tests evaluate to True and another
 5 tests evaluate to False."""
 
-animal = "monkey"
+animal: list = "monkey"
 
 print("Is animal == ape? I predict False!")
 print(animal == "ape")
@@ -173,19 +173,19 @@ print(animal == "lion")
 print("Is animal == monkey? I predict True!")
 print(animal == "monkey")
 
-animal = "dog"
+animal: str = "dog"
 print("Is animal == dog? I predict True!")
 print(animal == "dog")
 
-animal = "cat"
+animal: str = "cat"
 print("Is animal == cat? I predict True!")
 print(animal == "cat")
 
-animal = "duck"
+animal: str = "duck"
 print("Is animal == duck? I predict True!")
 print(animal == "duck")
 
-animal = "mouse"
+animal: str = "mouse"
 print("Is animal == mouse? I predict True!")
 print(animal == "mouse")
 
@@ -209,7 +209,7 @@ print("ciao" == "Ciao")
 print("Is ciao != Ciao? I predict True")
 print("ciao" != "Ciao")
 
-my_pizza = ['mozzarella', 'pepperone', 'fior di zucca', 'capriciosa']
+my_pizza: list = ['mozzarella', 'pepperone', 'fior di zucca', 'capriciosa']
 print("is salsiccia in the list? I predict False!")
 if "salsiccia" in my_pizza:
     print(True)
@@ -222,7 +222,7 @@ if "salsiccia" not in my_pizza:
 else:
     print(False)
 
-x = 100
+x: int = 100
 print("is X <= 50? I predict False")
 if x <= 50:
     print(True)
@@ -247,7 +247,7 @@ print("\n")
 • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
 • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.) """
 
-alien_color = 'green'
+alien_color: str = 'green'
 
 if alien_color == 'green':
     print('the color green means you earned 5 points!')
@@ -283,7 +283,7 @@ print("\n")
 • If the alien is red, print a message that the player earned 15 points.
 • Write three versions of this program, making sure each message is printed for the appropriate color alien."""
 
-alien_colorG = 'green'
+alien_colorG: str = 'green'
 if alien_colorG == 'green':
     print('you earned 5 points!')
 elif alien_colorG == 'yellow':
@@ -293,7 +293,7 @@ elif alien_colorG == 'red':
 else:
     print('you did not earn points!')
 
-alien_colorY = 'yellow'
+alien_colorY: str = 'yellow'
 if alien_colorY == 'green':
     print('you earned 5 points!')
 elif alien_colorY == 'yellow':
@@ -303,7 +303,7 @@ elif alien_colorY == 'red':
 else:
     print('you did not earn points!')
 
-alien_colorR = 'red'
+alien_colorR: str = 'red'
 if alien_colorR == 'green':
     print('you earned 5 points!')
 elif alien_colorR == 'yellow':
@@ -324,7 +324,7 @@ print("\n")
 • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
 • If the person is age 65 or older, print a message that the person is an elder."""
 
-age = 65
+age: int = 65
 
 if 0 <= age < 2:
     print("the person is a baby.")
@@ -346,7 +346,7 @@ print("\n")
 • Make a list of your three favorite fruits and call it favorite_fruits.
 • Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block should print a statement, such as You really like Apples!"""
 
-favorite_fruits = ["banana",  "orange", "pineapple"]
+favorite_fruits: list = ["banana",  "orange", "pineapple"]
 
 for fruit in favorite_fruits:
     if fruit == "blackberry":
@@ -367,7 +367,7 @@ print("\n")
 • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
 • Otherwise, print a generic greeting, such as Hello Jaden, thank you for logging in again."""
 
-usernames = ['admin', 'jdalmas', 'mfontevecchia', 'bandreucci', 'rdinatale']
+usernames: list = ['admin', 'jdalmas', 'mfontevecchia', 'bandreucci', 'rdinatale']
 
 if usernames:
     for user in usernames:
@@ -397,8 +397,8 @@ print("\n")
 • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
 • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted. (To do this, you’ll need to make a copy of current_users containing the lowercase versions of all existing users.) """
 
-usernames = ['admin', 'jdalmas', 'mfontevecchia', 'bandreucci', 'rdinatale']
-new_users = ['rfontevecchia', 'Jdalmas', 'apannonne', 'jvsoares', 'Rdinatale']
+usernames: list = ['admin', 'jdalmas', 'mfontevecchia', 'bandreucci', 'rdinatale']
+new_users: list = ['rfontevecchia', 'Jdalmas', 'apannonne', 'jvsoares', 'Rdinatale']
 
 for user in new_users:
     user = user.lower()
@@ -415,7 +415,7 @@ print("\n")
 • Loop through the list.
 • Use an if-elif-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line."" """
 
-numbers = []
+numbers: list = []
 
 for number in range(1,10):
     if number == 1:
