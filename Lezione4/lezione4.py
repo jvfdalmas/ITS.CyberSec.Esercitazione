b2d1e94304935f324b0ab9856d11ac57294cac6f
jvfdalmas <juaaaum@hotmail.com>
2024-04-24 13:47:08 +0200

    final

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -221,4 +221,4 @@ print(car2)
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n")
 
-"""8-17. Styling Functions: Choose any three programs you wrote for this chapter, and make sure they follow the styling guidelines described in this section."""
+"""8-17. Styling Functions: Choose any three programs you wrote for this chapter, and make sure they follow the styling guidelines described in this section,."""
