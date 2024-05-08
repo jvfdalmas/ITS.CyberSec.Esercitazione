784c4d0bff50d5c2e0972159ef5c84f62207e416
jvfdalmas <juaaaum@hotmail.com>
2024-05-08 09:39:01 +0200

    parcial

--- Lezione5/lezione5.py
+++ Lezione5/lezione5.py
@@ -1,11 +1,7 @@
-"""1. Create a Playlist:
-
-Write a function called create_playlist() that accepts a playlist name and a variable number of song titles. The function should return a dictionary with the playlist name and a set of songs. Call the function with different numbers of songs to demonstrate flexibility.
-
+"""1. Create a Playlist: Write a function called create_playlist() that accepts a playlist name and a variable number of song titles. The function should return a dictionary with the playlist name and a set of songs. Call the function with different numbers of songs to demonstrate flexibility.
 Example: create_playlist("Road Trip", {"Song 1", "Song 2"})
 
 Write a function called add_like() that accepts a dictionary, the name of a playlist, and a boolean value indicating whether it is liked (True or False). This function should return an updated dictionary.
-
 Example: add_like(dictionary, “Road Trip”, liked=True)"""
 
 def create_playlist(playlist_name: str, songs_titles: set) -> dict:
@@ -17,7 +13,6 @@ playlist2 = create_playlist("Chill Vibes", {"Song 3", "Song 4", "Song 5"})
 print(playlist1)
 print(playlist2)
 
-
 def add_like(playlists_dict: dict, playlist_name: str, liked: bool):
     for name in playlists_dict.keys():
         if name == playlist_name:
@@ -25,7 +20,6 @@ def add_like(playlists_dict: dict, playlist_name: str, liked: bool):
         else:
             playlists_dict[playlist_name] = []
             return playlists_dict
-
 
 updated_playlist = add_like(playlist1, "Road Trip", liked = True)
 print(updated_playlist)
@@ -35,14 +29,10 @@ print(updated_playlist)
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n") 
 
-"""2. Book Collection:
-
-Write a function called add_book() that accepts an author's name and a variable number of book titles authored by them. This function should return a dictionary where the author's name is the key and the value is a list of their books. Demonstrate this function by adding books for different authors.
-
+"""2. Book Collection: Write a function called add_book() that accepts an author's name and a variable number of book titles authored by them. This function should return a dictionary where the author's name is the key and the value is a list of their books. Demonstrate this function by adding books for different authors.
 Example: add_book("Mark Twain", ["The Adventures of Tom Sawyer", "Life on the Mississippi"])
 
 Write a function called delete_book() that accepts a dictionary and the name of the author from whom to remove all details. This function should return an updated dictionary.
-
 Example: delete_book(dictionary, “Mark Twain”)"""
 
 def add_book(author_name: str, books: list) -> dict:
@@ -63,10 +53,7 @@ print(MK_dict)
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n") 
 
-"""3. Personal Info:
-
-Write a build_profile() function that accepts the name , surname,  occupation, location, and age  of a person. Make occupation, location, and age optional parameters. Use this function to create profiles for different people, demonstrating how it handles these optional parameters.
-
+"""3. Personal Info: Write a build_profile() function that accepts the name , surname,  occupation, location, and age  of a person. Make occupation, location, and age optional parameters. Use this function to create profiles for different people, demonstrating how it handles these optional parameters.
 Example: build_profile("John", "Doe", occupation="Developer", location="USA", age=30)"""
 
 def build_profile(name: str, surname: str, occupation: str = None, location: str = None, age:int = None) -> dict:
@@ -95,14 +82,10 @@ print(JD_profile)
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n") 
 
-"""4. Event Organizer:
-
-Write a function called plan_event() that accepts an event name, a list of participants, and an hour. The function should return a dictionary that includes the event name and a list of the participants. Call this function with varying numbers of participants to plan different events.
-
+"""4. Event Organizer: Write a function called plan_event() that accepts an event name, a list of participants, and an hour. The function should return a dictionary that includes the event name and a list of the participants. Call this function with varying numbers of participants to plan different events.
 Example: plan_event("Code Workshop", ["Alice", "Bob", "Charlie"],”4pm”)
 
 Write a function called modify_event() that accepts a dictionary, an event name, and new details to modify an already planned event.
-
 Example: modify_event(dictionary, "Code Workshop", ["Alice", "Bob", "Charlie"], ”4pm”)"""
 
 def plan_event(event_name: str, participants: list, time: str) -> dict:
@@ -120,25 +103,19 @@ def modify_event(dict_events: dict, event_name: str, participants: list = None, 
             dict_events[event_name]["time of the event"] = time
         return dict_events
     else:
-        return "no change was made"
+        return "event not found!!!"
     
-
-modify_event(eventA, "D", ["Alice", "Bob", "Charlie"],"4pm")
-print(eventA)
+new_event_a = modify_event(eventA, "D", ["Alice", "Bob", "Charlie"],"4pm")
+print(new_event_a)
 
 modify_event(eventA, "Code Workshop", ["Alice", "Charlie"],"3pm")
 print(eventA)
 
-
 # -------------------------------------------------------------------------------------------------------------------------------
 print("\n") 
 
-"""5. Shopping List:
-
-Write a function called create_shopping_list() that accepts a store name and any number of items as arguments. It should return a dictionary with the store name and a set of items to buy there. Test the function with different stores and item lists.
-
+"""5. Shopping List: Write a function called create_shopping_list() that accepts a store name and any number of items as arguments. It should return a dictionary with the store name and a set of items to buy there. Test the function with different stores and item lists.
 Example: create_shopping_list("Grocery Store", {"Milk", "Eggs", "Bread"})
 
 Write a function called print_shopping_list() that accepts a dictionary and a store name, then prints each item from that store's shopping list.
-
 Example: print_shopping_list(dictionary, "Grocery Store")"""
