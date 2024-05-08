8114033d1779c25ce5058ad1dc08bd3c5f303b85
jvfdalmas <juaaaum@hotmail.com>
2024-05-08 13:48:20 +0200

    parcial

--- Lezione6/lezione6.py
+++ Lezione6/lezione6.py
@@ -2,11 +2,16 @@
 
 class Person:
 
-    def __init__(self, name: str, surname: str, taxpayers_id: str) -> None:
+    def __init__(self, name: str, surname: str, taxpayers_id: str, birthday: str, birth_place: str, gender: str) -> None:
         ID: int = 1  # come ID non è sotto self, Python non riconosce come attributo della classe e non la stampa 
         self._name: str = name # attributo della classe     # per convenzione, sempre che c'è un "_" significa attributo privato
         self.surname: str = surname # attributo della classe
-        self._taxpayers_id: str = taxpayers_id # attributo della classe
+        self._birthday: str = birthday
+        self._brith_place: str = birth_place
+        self._gender: str = gender
+        self._taxpayers_id: None = None
+        self.compute_taxpayers_id()
+
 
     def get_name(self): # "get" serve per l'utente esterno aver accesso alla informazione privata
         """This function gets the attribute name.
@@ -34,7 +39,18 @@ class Person:
         input: taxpayers_id: str, the parameter contains the user's taxpayer's id
         return: None"""
         
-        self._taxpayers_id = taxpayers_id   
+        self._taxpayers_id = taxpayers_id
+
+    def compute_taxpayers_id(self) -> str:
+        self._taxpayers_id: str = "calcolare il CF!"
+        pass
+    
+    def check_taxpayers_id(self, taxpayers_id: str) -> bool:
+        """This function checks the taxpayer's id.
+        input: taxpayers_id: str, the parameter contains the user's taxpayer's id
+        return: boolean"""
+        
+        pass   
      
     def __str__(self) -> str:
         """This function prints the attributes in the form of a str.
@@ -50,21 +66,21 @@ class Person:
 
         return value.hash() == self.hash()
     
-persona_1: Person = Person(name="Flavio", surname="Giorgi", taxpayers_id="DGDFVR")
-persona_2: Person = Person(name="Joao", surname="Dal Mas", taxpayers_id="DMJVF")
+persona_1: Person = Person(name="Flavio", surname="Giorgi", taxpayers_id="DGDFVR", birthday="24/12/1994", birth_place="Roma", gender="Male")
+#persona_2: Person = Person(name="Joao", surname="Dal Mas", taxpayers_id="DMJVF")
 
 print(persona_1._name, persona_1.surname)
-print(persona_2._name, persona_2.surname)
+#print(persona_2._name, persona_2.surname)
 
 print(persona_1.get_name())
 print(str(persona_1))
-print(str(persona_2))
+#print(str(persona_2))
 
-queue: list = [persona_1, persona_2]
-for person in queue:
-    persons_name = person.get_name()
-    persons_tax_id = person.get_taxpayers_id()
-    print(f"The taxpayers id of {persons_name} is {persons_tax_id}")
+#queue: list = [persona_1, persona_2]
+#for person in queue:
+#    persons_name = person.get_name()
+#    persons_tax_id = person.get_taxpayers_id()
+#    print(f"The taxpayers id of {persons_name} is {persons_tax_id}")
 
 persona_1.set_taxpayers_id("FFFF")
 print(persona_1.get_taxpayers_id())
