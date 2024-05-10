7e1c04f8bf3a5c5975fe799513b2ec5e37224de4
Joao Victor Dal Mas <163042058+jvfdalmas@users.noreply.github.com>
2024-05-10 16:47:13 +0200

    final

--- Lezione7/valutazione.py
+++ Lezione7/valutazione.py
@@ -14,10 +14,13 @@ def rimuovi_elementi(lista: list[int], da_rimuovere: dict[int:int]) -> list[int]
                 lista.remove(elemento)
                 da_rimuovere[elemento] -= 1
     return lista
-    pass
+
 
 print(rimuovi_elementi([1, 2, 3, 2, 4], {2: 2})) #[1, 3, 4]
 print(rimuovi_elementi([], {2: 1})) #[]
+
+# -------------------------------------------------------------------------------------------------------------------------------
+print("\n") 
 
 """2. Scrivi una funzione che prenda in input una lista di dizionari che rappresentano voti di studenti e aggrega i voti per studente in un nuovo dizionario.
 
@@ -34,22 +37,21 @@ def aggrega_voti(voti: list[dict]) -> dict[str:list[int]]:
     if len(voti) == 0:
         return dict_finale
     else:
-        for item in voti:
-            for key, value in item.items():
+        for element in voti:
+            for key, value in element.items():
                 if key == "nome":
                     if value not in dict_finale:
-                        dict_finale[value] = []
-                elif key == "voto":
-                    for name in dict_finale.keys():
+                        dict_finale[value] = [element["voto"]]
+                    elif value in dict_finale:
+                        dict_finale[value].append(element["voto"])
 
-                        dict_finale[name] += [int(value)]
-                        print(dict_finale, "elif cycle")
     return dict_finale
-
-    pass
 
 print(aggrega_voti([{'nome': 'Alice', 'voto': 90}, {'nome': 'Bob', 'voto': 75}, {'nome': 'Alice', 'voto': 85}])) # {'Alice': [90, 85], 'Bob': [75]}
 print(aggrega_voti([]))  #{}
+
+# -------------------------------------------------------------------------------------------------------------------------------
+print("\n") 
 
 """ 3. Scrivi una funzione che accetti un dizionario di prodotti con i prezzi e restituisca un nuovo dizionario con solo i prodotti che hanno un prezzo superiore a 20, scontati del 10%.
 
@@ -65,7 +67,7 @@ def filtra_e_mappa(prodotti) -> dict:
     dict_risultato: dict = {} 
     for key, value in prodotti.items():
         if value > 20:
-            scontato = value * 0.9
+            scontato: float = value * 0.9
             dict_risultato[key] = scontato
 
     return dict_risultato
@@ -74,6 +76,9 @@ def filtra_e_mappa(prodotti) -> dict:
 
 print(filtra_e_mappa({'Penna': 15.0, 'Zaino': 50.0, 'Quaderno': 22.0})) # {'Zaino': 45.0, 'Quaderno': 19.8}
 print(filtra_e_mappa({'Gomma': 2.0, 'Matita': 1.0})) #{}
+
+# -------------------------------------------------------------------------------------------------------------------------------
+print("\n") 
 
 """ 4.
 PARTE 1
@@ -103,12 +108,10 @@ def create_contact(name: str, email: str=None, telefono: int=None) -> dict:
         create_contact_list["telefono"] = None
 
     return create_contact_list
-    pass
-
 
 
 def update_contact(dictionary: dict, name: str, email: str =None, telefono: int=None) -> dict:
-    updated_dictionary = dictionary
+    updated_dictionary: dict = dictionary
 
     if email is not None:
         updated_dictionary["email"] = email
@@ -116,7 +119,6 @@ def update_contact(dictionary: dict, name: str, email: str =None, telefono: int=
         updated_dictionary["telefono"] = telefono
 
     return updated_dictionary
-    pass
 
 contact = create_contact("Mario Rossi", email="mario.rossi@gmail.com", telefono=788787)
 print(create_contact("Mario Rossi", email="mario.rossi@gmail.com", telefono=788787)) # {'profile': 'Mario Rossi', 'email': 'mario.rossi@gmail.com', 'telefono': 788787}
