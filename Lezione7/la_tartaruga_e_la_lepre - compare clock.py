e659256348a73ccdcca20c834e2bf3c49951a833
Joao Victor Dal Mas <163042058+jvfdalmas@users.noreply.github.com>
2024-05-14 21:28:32 +0200

    final

--- Lezione7/la_tartaruga_e_la_lepre - compare clock.py
+++ Lezione7/la_tartaruga_e_la_lepre - compare clock.py
@@ -32,79 +32,72 @@ Requisiti del Codice:
 - Implementare un loop per simulare i tick dell'orologio. Ad ogni tick, calcolare le mosse, mostrare la posizione sulla corsia di gara, e determinare l'eventuale fine dell
 a gara. """
 
-import random
+from random import choices
 
-race_track_len: int = 70 # percorso della corsa
-race_track: list = ["_" for position in range(0, race_track_len)] # corsa
-tortoise: int = 1 # posizione iniziale - tortoise
-rabbit: int = 1 # posizione iniziale - rabbit
-clock: int = 0 # counter di mose | secondi
+def turtle_race():
+    turtle_steps:list = [3, -6, 1]
+    turtle_steps_probability:list = [0.5, 0.2, 0.3]
+    turtle_move = choices(turtle_steps, turtle_steps_probability)
+    turtle_track_race:list = [numero for numero in range(1,71)]
+    turtle_clock: int = 0
+    turtle_track_race[0] = "turtle"
 
-def tortoise_move() -> int:
-    i: int = random.randint(1,10)
+    while turtle_track_race[69] != "turtle":
+        
+        turtle_move: list = choices(turtle_steps, turtle_steps_probability)
+        turtle_move: int = int(turtle_move[0])
 
-    if 1 <= i <= 5: # 50% di probabilità
-        return 3 # Passo veloce: avanza di 3 quadrati
-    elif 6 <= i <= 7: # 20% di probabilità
-        return -6 # Scivolata: arretra di 6 quadrati. Non può andare sotto il quadrato 1.
-    elif 7 <= i <= 10: # 30% di probabilità
-        return 1 # Passo lento: avanza di 1 quadrato
+        last_index: int = turtle_track_race.index("turtle")
+        
+        turtle_track_race[last_index] = last_index + 1
+        turtle_clock += 1
+        
+        if last_index + turtle_move <= 0:
+            turtle_track_race[0] = "turtle"
+        if last_index + turtle_move >= 69:
+            turtle_track_race[69] = "turtle"
+        else:
+            turtle_track_race[last_index + turtle_move] = "turtle"
+        
+    return turtle_clock
 
-def rabbit_move():
-    i: int = random.randint(1,10)
+def rabbit_race():
+    rabbit_steps:list = [3, -6, 1]
+    rabbit_steps_probability:list = [0.5, 0.2, 0.3]
+    rabbit_move = choices(rabbit_steps, rabbit_steps_probability)
+    rabbit_track_race:list = [numero for numero in range(1,71)]
+    rabbit_clock: int = 0
+    rabbit_track_race[0] = "rabbit"
 
-    if 1 <= i <= 2: # 20% di probabilità
-        return 0# non si muove.
-    elif 3 <= i <= 4: # 20% di probabilità
-        return 9 # grande balzo: avanza di 9 quadrati
-    elif i == 5: # 10% di probabilità
-        return -12 # grande scivolata: arretra di 12 quadrati
-    elif 6 <= i <= 8: # 20% di probabilità
-        return 1 # piccolo balzo: avanza di 1 quadrato
-    elif 9 <= i <= 10: # 20% di probabilità
-        return -2 # piccola scivolata: arretra di 2 quadrati
+    while rabbit_track_race[69] != "rabbit":
+        
+        rabbit_move: list = choices(rabbit_steps, rabbit_steps_probability)
+        rabbit_move: int = int(rabbit_move[0])
+        last_index: int = rabbit_track_race.index("rabbit")
+        rabbit_track_race[last_index] = last_index + 1
+        rabbit_clock += 1
+        
+        if last_index + rabbit_move <= 0:
+            rabbit_track_race[0] = "rabbit"
+        if last_index + rabbit_move >= 69:
+            rabbit_track_race[69] = "rabbit"
+        else:
+            rabbit_track_race[last_index + rabbit_move] = "rabbit"
+        
+    return rabbit_clock
 
-def corsia_di_gara(race_track: list, rabbit: int, tortoise: int):
-    # cleans the last position
-    for i in range(len(race_track)):
-        if race_track[i] in ['OUCH!!!', 'H', 'T']:
-            race_track[i] = '_'
-    
-    # rabbit stays inside the race track
-    rabbit = max(1, rabbit)
-    rabbit = min(rabbit, 70)
-    
-    # tortoise stays inside the race track
-    tortoise = max(1, tortoise)
-    tortoise = min(tortoise, 70)
+def rabbit_turtle_race():
+    clock_rabbit_race: int = rabbit_race()
+    clock_turtle_race: int = turtle_race()
 
-    # update racers position in race track
-    if rabbit == tortoise:
-        race_track[rabbit-1] = 'OUCH!!!'
-    else:
-        race_track[rabbit-1] = "H"
-        race_track[tortoise-1] = "T"
+    if clock_rabbit_race == clock_turtle_race:
+        return f"Both turtle and rabbit arrived together after {clock_rabbit_race} seconds. It is a tie!"
+    elif clock_rabbit_race <= clock_turtle_race:
+        return f"Rabbit won!!!! It took rabbit {clock_rabbit_race} seconds, while the turtle arrived after {clock_turtle_race} seconds."
+    elif clock_rabbit_race >= clock_turtle_race:
+        return f"Turtle won!!! It took rabbit {clock_rabbit_race} seconds, while the turtle arrived after {clock_turtle_race} seconds."
 
-    # prints updated race track
-    print(" ".join(race_track), f"rabbit position: {rabbit} / tortoise position: {tortoise}")
-    
-    # prints the winner
-    if rabbit == 70 and tortoise == 70:
-        print("IT'S A TIE.")
-    elif race_track[69] == "T":
-        print("TORTOISE WINS! || VAY!!!")
-    elif race_track[69] == "H":
-        print("HARE WINS || YUCH!!!")
-
-print("BANG !!!!! AND THEY'RE OFF !!!!!")
-
-while race_track[69] == "_":
-    clock += 1
-    rabbit += rabbit_move()
-    tortoise += tortoise_move()
-    corsia_di_gara(race_track, rabbit, tortoise)
-
-print(f"The race took {clock} seconds")
+print(rabbit_turtle_race())
 
 
 
