93dd369e72aefa49619e7b464f67d8450e97658e
jvfdalmas <juaaaum@hotmail.com>
2024-06-21 10:10:28 +0200

    completo

--- Lezione15/Lista_Concatenata/lista_concatenata.py
+++ Lezione15/Lista_Concatenata/lista_concatenata.py
@@ -1,15 +1,54 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
+class Node:
+
+    def __init__(self, val, next=None):
         self.val = val
         self.next = next
-        
-def reverse_list(head: ListNode) -> list[int]:
-    current = head
-    risult = []
-    risult.append(current.val)
-    while current.next is not None:
-        current = current.next
-        risult.append(current.val)
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def insert(self, val):
+        node = Node(val)
+
+        if self.head == None:
+            self.head = node
+            return 
+        else:
+            curr = self.head
+
+        while True:
+            if curr.next == None:
+                curr.next = node
+                return
+            else:
+                curr = curr.next
+
+    def print(self):
+        curr = self.head
+
+        while True:
+            if curr != None:
+                print(curr.val, " -> ", end="")
+                curr = curr.next 
+            else:
+                print("None")
+                break
+
+linkedlist = LinkedList()
+linkedlist.insert(0)
+linkedlist.insert(1)
+linkedlist.insert(2)
+linkedlist.insert(3)
+linkedlist.print()
+
+def reversed_linked_list(node: Node) -> str: 
+    curr = node
+    if curr != None:
+        helper = str(reversed_linked_list(curr.next)), " -> ", str(curr.val)
+        return "".join(helper)
     
-    risult.reverse()
-    return risult
+print(reversed_linked_list(linkedlist.head))
+
+
